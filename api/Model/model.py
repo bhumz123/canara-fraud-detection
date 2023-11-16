0704ccdad3fe64423fa940646e9f4fb2c6674706
Bhumika Rana <97088096+bhumz123@users.noreply.github.com>
2023-11-16 10:52:54 +0530

    Update model.py

--- api/Model/model.py
+++ api/Model/model.py
@@ -9,9 +9,9 @@ import random
 lstm_model = keras.models.load_model('api/Model/LSTM_model.h5')
 # ARF model
 # json_data = json.load(fp=open("/Users/as/TanuProjects/amex_project/testing_data.json"))
-'''import pickle
+import pickle
 with open('api/Model/ARF_evaluator.pkl','rb') as file:
-    arf_evaluator=pickle.load(file)'''
+    arf_evaluator=pickle.load(file)
 
 def lstm_predictions(df):
     features = np.array(df)
@@ -21,20 +21,20 @@ def lstm_predictions(df):
     print(lstm_y_pred_binary)
     return lstm_y_pred_binary
 
-'''def arf_pred(df):
+def arf_pred(df):
     data = np.array(df)
     arf_pred=arf_evaluator.predict(data)
     arf_predictions=np.array(arf_pred[0])
     arf_predtn= np.reshape(arf_predictions,(-1,1))
     print(arf_predtn)
-    return arf_predtn'''
+    return arf_predtn
 
 def combined_predctns(df):
     lstm_predtns=lstm_predictions(df)
-   # arf_predtns=arf_pred(df)
+    arf_predtns=arf_pred(df)
     lstm_weight= 0.6
-    #arf_weight=0.4
-    #combined_predictions= ((lstm_weight * lstm_predtns + arf_weight * arf_predtns)/(lstm_weight+arf_weight))
+    arf_weight=0.4
+    combined_predictions= ((lstm_weight * lstm_predtns + arf_weight * arf_predtns)/(lstm_weight+arf_weight))
     #print(combined_predictions)
     combined_predictions = (lstm_predtns > 0.5).astype(int)
     return combined_predictions
